bd128e211c56f4fef42bdc2f01530f6dda9398d7
Lucas Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-07-21 22:23:33 +0800

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,40 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int p1[2], p2[2];
+    char tmp;
+
+    if (argc != 1) {
+        fprintf(2, "Usage: pingpong\n");
+        exit(1);
+    }
+
+    pipe(p1);
+    pipe(p2);
+
+    if (fork() == 0) {
+        close(p1[1]);
+        read(p1[0], &tmp, 1);
+        close(p1[0]);
+        fprintf(1, "%d: received ping\n", getpid());
+
+        close(p2[0]);
+        write(p2[1], "0", 1);
+        close(p2[1]);
+
+    } else {
+        close(p1[0]);
+        write(p1[1], "0", 1);
+        close(p1[1]);
+
+        close(p2[1]);
+        read(p2[0], &tmp, 1);
+        close(p2[0]);
+        fprintf(1, "%d: received pong\n", getpid());
+    }
+    exit(0);
+}
