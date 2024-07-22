bf55cb6710df48696a4541914c9758215d6c6f95
Lucas Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-07-22 18:24:23 +0800

    modify pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,6 +2,31 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+static void
+create_pipe(int *p)
+{
+    if (pipe(p) < 0) {
+        fprintf(2, "pipe failed\n");
+        exit(1);
+    }
+}
+
+static void
+send(int *p)
+{
+    close(p[0]);
+    write(p[1], "0", 1);
+    close(p[1]);
+}
+
+static void
+receive(int *p, char *tmp)
+{
+    close(p[1]);
+    read(p[0], tmp, 1);
+    close(p[0]);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -13,27 +38,20 @@ main(int argc, char *argv[])
         exit(1);
     }
 
-    pipe(p1);
-    pipe(p2);
+    create_pipe(p1);
+    create_pipe(p2);
 
     if (fork() == 0) {
-        close(p1[1]);
-        read(p1[0], &tmp, 1);
-        close(p1[0]);
+        /* ping */
+        receive(p1, &tmp);
         fprintf(1, "%d: received ping\n", getpid());
-
-        close(p2[0]);
-        write(p2[1], "0", 1);
-        close(p2[1]);
-
+        /* pong */
+        send(p2);
     } else {
-        close(p1[0]);
-        write(p1[1], "0", 1);
-        close(p1[1]);
-
-        close(p2[1]);
-        read(p2[0], &tmp, 1);
-        close(p2[0]);
+        /* ping */
+        send(p1);
+        /* pong */
+        receive(p2, &tmp);
         fprintf(1, "%d: received pong\n", getpid());
     }
     exit(0);
